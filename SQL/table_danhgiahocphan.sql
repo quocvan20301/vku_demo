def86208974394678d2bf2fe1a3fba5a9ad007f6
quocvan20301 <pqvan.19it3@vku.udn.vn>
2021-04-28 09:22:36 +0700

    sql2

--- SQL/table_danhgiahocphan.sql
+++ SQL/table_danhgiahocphan.sql
@@ -1,39 +1,78 @@
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (198, 'Điểm danh', 100, '5', 5, 1, 111);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (199, 'Trình bày tại lớp/Trắc nghiệm', 30, '1', 6, 2, 111);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (200, 'Cuốn báo cáo và trình bày tại lớp', 70, '1_2_4', 7, 2, 111);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (201, 'Tự luận', 100, '3_4', 8, 3, 111);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (202, 'Tự luận', 50, '1_2_3', 9, 4, 111);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (203, 'Báo cáo; Hỏi & Đáp', 50, '1_3_4', 10, 4, 111);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (209, 'Dựa vào % vắng học trên hệ thống điểm danh', 100, '1_2_3_4_5', 5, 1, 85);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (210, 'Thực hành', 100, '1_2_3_4_5_6_7_8', 6, 2, 85);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (211, 'Trắc nghiệm', 100, '1_2_3_4_5', 8, 3, 85);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (212, 'Báo cáo đồ án môn học', 100, '1_2_3_4_5_6_7_8', 9, 4, 85);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (213, 'Trình bày tại lớp/Trắc nghiệm', 30, '1_2', 6, 2, 112);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (214, 'Cuốn báo cáo và trình bày tại lớp', 70, '1_2_3', 7, 2, 112);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (215, 'Tự luận', 100, '1_2_3', 8, 3, 112);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (216, 'Bài tập lớn', 100, '1_2_3_4', 9, 4, 112);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (217, 'Điểm danh', 100, '5', 5, 1, 91);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (218, 'Bài thực hành', 100, '1_2_5', 6, 2, 91);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (219, 'Xây dựng ứng dụng', 100, '1_2_3_4_5', 8, 3, 91);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (220, 'Tự luận', 100, '1_2_3_4_5', 9, 4, 91);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (221, 'Kiểm tra chuyên cần', 100, '6', 5, 1, 114);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (222, 'Chấm bài kiểm tra', 100, '6', 6, 2, 114);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (223, 'Viết', 100, '1_2_3_4_7', 8, 3, 114);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (224, 'Vấn đáp', 50, '1_2_3_4_5_6_7', 9, 4, 114);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (225, 'Bài tập nhóm', 50, '1_2_3_4_5_6_7', 10, 4, 114);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (226, 'Điểm danh', 100, '5', 5, 1, 47);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (227, 'Trình bày tại lớp', 100, '1_2_3_4', 6, 2, 47);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (228, 'Tự luận', 100, '1_2_3', 8, 3, 47);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (229, 'Bài tập lớn', 100, '1_2_3_4_5', 9, 4, 47);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (230, 'Dựa vào % vắng học trên hệ thống điểm danh', 100, '1_2_3_4_5_6', 5, 1, 21);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (231, 'Bài tập quá trình', 100, '1_2_3_4_5_6', 6, 2, 21);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (232, 'Kiểm tra trắc nghiệm', 100, '1_2_3', 8, 3, 21);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (233, 'Thi trắc nghiệm', 100, '1_2_3_4_5_6', 9, 4, 21);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (234, 'Điểm danh', 100, '5', 5, 1, 19);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (235, 'Bài tập', 100, '1_2_3_4_5', 6, 2, 19);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (236, 'Lý thuyết', 100, '1_2', 8, 3, 19);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (237, 'Vấn đáp', 100, '1_2_3_4_5', 9, 4, 19);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (238, 'Điểm danh', 100, '1', 5, 1, 51);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (239, 'Trình bày tại lớp', 100, '1_2_3_4', 6, 2, 51);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (240, 'Bài thực hành', 100, '1_2', 8, 3, 51);
-INSERT INTO `table_danhgiahocphan`(`id`, `phuongphapdanhgia`, `trongsobaidanhgia`, `cdr_hocphan`, `id_baidanhgia`, `id_baidanhgia_parent`, `id_hocphan`) VALUES (241, 'Cuốn báo cáo và trình bày tại lớp', 100, '1_2_3_4_5', 9, 4, 51);
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Mysql_DB
+ Source Server Type    : MySQL
+ Source Server Version : 100417
+ Source Host           : localhost:3306
+ Source Schema         : demovku
+
+ Target Server Type    : MySQL
+ Target Server Version : 100417
+ File Encoding         : 65001
+
+ Date: 28/04/2021 09:19:00
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for table_danhgiahocphan
+-- ----------------------------
+DROP TABLE IF EXISTS `table_danhgiahocphan`;
+CREATE TABLE `table_danhgiahocphan`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `phuongphapdanhgia` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `trongsobaidanhgia` int NULL DEFAULT NULL,
+  `cdr_hocphan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id_baidanhgia` int NULL DEFAULT NULL,
+  `id_baidanhgia_parent` int NULL DEFAULT NULL,
+  `id_hocphan` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 242 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of table_danhgiahocphan
+-- ----------------------------
+INSERT INTO `table_danhgiahocphan` VALUES (198, 'Điểm danh', 100, '5', 5, 1, 111);
+INSERT INTO `table_danhgiahocphan` VALUES (199, 'Trình bày tại lớp/Trắc nghiệm', 30, '1', 6, 2, 111);
+INSERT INTO `table_danhgiahocphan` VALUES (200, 'Cuốn báo cáo và trình bày tại lớp', 70, '1_2_4', 7, 2, 111);
+INSERT INTO `table_danhgiahocphan` VALUES (201, 'Tự luận', 100, '3_4', 8, 3, 111);
+INSERT INTO `table_danhgiahocphan` VALUES (202, 'Tự luận', 50, '1_2_3', 9, 4, 111);
+INSERT INTO `table_danhgiahocphan` VALUES (203, 'Báo cáo; Hỏi & Đáp', 50, '1_3_4', 10, 4, 111);
+INSERT INTO `table_danhgiahocphan` VALUES (209, 'Dựa vào % vắng học trên hệ thống điểm danh', 100, '1_2_3_4_5', 5, 1, 85);
+INSERT INTO `table_danhgiahocphan` VALUES (210, 'Thực hành', 100, '1_2_3_4_5_6_7_8', 6, 2, 85);
+INSERT INTO `table_danhgiahocphan` VALUES (211, 'Trắc nghiệm', 100, '1_2_3_4_5', 8, 3, 85);
+INSERT INTO `table_danhgiahocphan` VALUES (212, 'Báo cáo đồ án môn học', 100, '1_2_3_4_5_6_7_8', 9, 4, 85);
+INSERT INTO `table_danhgiahocphan` VALUES (213, 'Trình bày tại lớp/Trắc nghiệm', 30, '1_2', 6, 2, 112);
+INSERT INTO `table_danhgiahocphan` VALUES (214, 'Cuốn báo cáo và trình bày tại lớp', 70, '1_2_3', 7, 2, 112);
+INSERT INTO `table_danhgiahocphan` VALUES (215, 'Tự luận', 100, '1_2_3', 8, 3, 112);
+INSERT INTO `table_danhgiahocphan` VALUES (216, 'Bài tập lớn', 100, '1_2_3_4', 9, 4, 112);
+INSERT INTO `table_danhgiahocphan` VALUES (217, 'Điểm danh', 100, '5', 5, 1, 91);
+INSERT INTO `table_danhgiahocphan` VALUES (218, 'Bài thực hành', 100, '1_2_5', 6, 2, 91);
+INSERT INTO `table_danhgiahocphan` VALUES (219, 'Xây dựng ứng dụng', 100, '1_2_3_4_5', 8, 3, 91);
+INSERT INTO `table_danhgiahocphan` VALUES (220, 'Tự luận', 100, '1_2_3_4_5', 9, 4, 91);
+INSERT INTO `table_danhgiahocphan` VALUES (221, 'Kiểm tra chuyên cần', 100, '6', 5, 1, 114);
+INSERT INTO `table_danhgiahocphan` VALUES (222, 'Chấm bài kiểm tra', 100, '6', 6, 2, 114);
+INSERT INTO `table_danhgiahocphan` VALUES (223, 'Viết', 100, '1_2_3_4_7', 8, 3, 114);
+INSERT INTO `table_danhgiahocphan` VALUES (224, 'Vấn đáp', 50, '1_2_3_4_5_6_7', 9, 4, 114);
+INSERT INTO `table_danhgiahocphan` VALUES (225, 'Bài tập nhóm', 50, '1_2_3_4_5_6_7', 10, 4, 114);
+INSERT INTO `table_danhgiahocphan` VALUES (226, 'Điểm danh', 100, '5', 5, 1, 47);
+INSERT INTO `table_danhgiahocphan` VALUES (227, 'Trình bày tại lớp', 100, '1_2_3_4', 6, 2, 47);
+INSERT INTO `table_danhgiahocphan` VALUES (228, 'Tự luận', 100, '1_2_3', 8, 3, 47);
+INSERT INTO `table_danhgiahocphan` VALUES (229, 'Bài tập lớn', 100, '1_2_3_4_5', 9, 4, 47);
+INSERT INTO `table_danhgiahocphan` VALUES (230, 'Dựa vào % vắng học trên hệ thống điểm danh', 100, '1_2_3_4_5_6', 5, 1, 21);
+INSERT INTO `table_danhgiahocphan` VALUES (231, 'Bài tập quá trình', 100, '1_2_3_4_5_6', 6, 2, 21);
+INSERT INTO `table_danhgiahocphan` VALUES (232, 'Kiểm tra trắc nghiệm', 100, '1_2_3', 8, 3, 21);
+INSERT INTO `table_danhgiahocphan` VALUES (233, 'Thi trắc nghiệm', 100, '1_2_3_4_5_6', 9, 4, 21);
+INSERT INTO `table_danhgiahocphan` VALUES (234, 'Điểm danh', 100, '5', 5, 1, 19);
+INSERT INTO `table_danhgiahocphan` VALUES (235, 'Bài tập', 100, '1_2_3_4_5', 6, 2, 19);
+INSERT INTO `table_danhgiahocphan` VALUES (236, 'Lý thuyết', 100, '1_2', 8, 3, 19);
+INSERT INTO `table_danhgiahocphan` VALUES (237, 'Vấn đáp', 100, '1_2_3_4_5', 9, 4, 19);
+INSERT INTO `table_danhgiahocphan` VALUES (238, 'Điểm danh', 100, '1', 5, 1, 51);
+INSERT INTO `table_danhgiahocphan` VALUES (239, 'Trình bày tại lớp', 100, '1_2_3_4', 6, 2, 51);
+INSERT INTO `table_danhgiahocphan` VALUES (240, 'Bài thực hành', 100, '1_2', 8, 3, 51);
+INSERT INTO `table_danhgiahocphan` VALUES (241, 'Cuốn báo cáo và trình bày tại lớp', 100, '1_2_3_4_5', 9, 4, 51);
+
+SET FOREIGN_KEY_CHECKS = 1;
--- SQL/table_trongso_lhp.sql
+++ SQL/table_trongso_lhp.sql
@@ -1,35 +1,75 @@
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6011, 111, 1, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6012, 111, 2, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6013, 111, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6014, 111, 4, 0.50, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6019, 85, 1, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6020, 85, 2, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6021, 85, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6022, 85, 4, 0.50, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6023, 112, 2, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6024, 112, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6025, 112, 4, 0.60, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6026, 91, 1, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6027, 91, 2, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6028, 91, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6029, 91, 4, 0.50, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6030, 114, 1, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6031, 114, 2, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6032, 114, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6033, 114, 4, 0.50, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6034, 47, 1, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6035, 47, 2, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6036, 47, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6037, 47, 4, 0.60, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6038, 21, 1, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6039, 21, 2, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6040, 21, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6041, 21, 4, 0.50, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6042, 19, 1, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6043, 19, 2, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6044, 19, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6045, 19, 4, 0.50, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6046, 51, 1, 0.10, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6047, 51, 2, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6048, 51, 3, 0.20, 0, NULL, NULL, NULL);
-INSERT INTO `table_trongso_lhp`(`id`, `id_lhp`, `id_trongso`, `trongso`, `trangthai`, `remember_token`, `created_at`, `updated_at`) VALUES (6049, 51, 4, 0.50, 0, NULL, NULL, NULL);
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Mysql_DB
+ Source Server Type    : MySQL
+ Source Server Version : 100417
+ Source Host           : localhost:3306
+ Source Schema         : demovku
+
+ Target Server Type    : MySQL
+ Target Server Version : 100417
+ File Encoding         : 65001
+
+ Date: 28/04/2021 09:21:19
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for table_trongso_lhp
+-- ----------------------------
+DROP TABLE IF EXISTS `table_trongso_lhp`;
+CREATE TABLE `table_trongso_lhp`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_lhp` int NOT NULL,
+  `id_trongso` int NOT NULL,
+  `trongso` double(11, 2) NULL DEFAULT NULL,
+  `trangthai` tinyint(1) NOT NULL,
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `created_at` timestamp(0) NULL DEFAULT NULL,
+  `updated_at` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6050 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of table_trongso_lhp
+-- ----------------------------
+INSERT INTO `table_trongso_lhp` VALUES (6011, 111, 1, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6012, 111, 2, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6013, 111, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6014, 111, 4, 0.50, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6019, 85, 1, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6020, 85, 2, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6021, 85, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6022, 85, 4, 0.50, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6023, 112, 2, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6024, 112, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6025, 112, 4, 0.60, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6026, 91, 1, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6027, 91, 2, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6028, 91, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6029, 91, 4, 0.50, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6030, 114, 1, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6031, 114, 2, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6032, 114, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6033, 114, 4, 0.50, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6034, 47, 1, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6035, 47, 2, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6036, 47, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6037, 47, 4, 0.60, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6038, 21, 1, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6039, 21, 2, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6040, 21, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6041, 21, 4, 0.50, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6042, 19, 1, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6043, 19, 2, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6044, 19, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6045, 19, 4, 0.50, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6046, 51, 1, 0.10, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6047, 51, 2, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6048, 51, 3, 0.20, 0, NULL, NULL, NULL);
+INSERT INTO `table_trongso_lhp` VALUES (6049, 51, 4, 0.50, 0, NULL, NULL, NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
